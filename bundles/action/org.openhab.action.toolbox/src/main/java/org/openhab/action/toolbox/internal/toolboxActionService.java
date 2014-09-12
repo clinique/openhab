bd08fb75d86b3572ad9b487bbf1e1c20f9efd6b0
clinique <glhopital@gmail.com>
2014-09-12 13:12:21 +0200

    First definition of a toolbox library

---
+++ bundles/action/org.openhab.action.toolbox/src/main/java/org/openhab/action/toolbox/internal/toolboxActionService.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2010-2013, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.action.toolbox.internal;
+
+import java.util.Dictionary;
+
+import org.openhab.core.scriptengine.action.ActionService;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+	
+
+/**
+ * This class registers an OSGi service for the toolbox action.
+ * 
+ * @author clinique
+ * @since 1.6.0
+ */
+public class toolboxActionService implements ActionService, ManagedService {
+
+	private static final Logger logger = LoggerFactory.getLogger(toolboxActionService.class);
+
+	/**
+	 * Indicates whether this action is properly configured which means all
+	 * necessary configurations are set. This flag can be checked by the
+	 * action methods before executing code.
+	 */
+	/* default */ static boolean isProperlyConfigured = false;
+	
+	public toolboxActionService() {
+	}
+	
+	public void activate() {
+	}
+	
+	public void deactivate() {
+		// deallocate Resources here that are no longer needed and 
+		// should be reset when activating this binding again
+	}
+
+	@Override
+	public String getActionClassName() {
+		return toolbox.class.getCanonicalName();
+	}
+
+	@Override
+	public Class<?> getActionClass() {
+		return toolbox.class;
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	public void updated(Dictionary<String, ?> config) throws ConfigurationException {
+		if (config != null) {
+			
+			// read config parameters here ...
+
+			isProperlyConfigured = true;
+		}
+	}
+	
+}
